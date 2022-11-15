b41efeae55efb4e01821b4519e355044c310143b
Vinicius Victor <vine.victordev@gmail.com>
2022-11-15 20:00:29 -0300

    Criação Classe Livro e Adicionado Objetos

---
+++ script.js
@@ -0,0 +1,30 @@
+class Livro {
+    Titulo
+    Autor
+    Editora
+    AnoPublicacao
+    Disponibilidade
+}
+
+let livroUm = new Livro()
+livroUm.Titulo = "O Guerreiro"
+livroUm.Autor = "Mark Audren"
+livroUm.Editora = "Panene"
+livroUm.AnoPublicacao = 2001
+livroUm.Disponibilidade = true
+
+let livroDois = new Livro()
+livroDois.Titulo = "O Samurai"
+livroDois.Autor = "Luther Jax"
+livroDois.Editora = "The Editor"
+livroDois.AnoPublicacao = 2010
+livroDois.Disponibilidade = true
+
+let livroTres = new Livro()
+livroTres.Titulo = "Vikings"
+livroTres.Autor = "Ragnar"
+livroTres.Editora = "Kattegat Editing"
+livroTres.AnoPublicacao = 1993
+livroTres.Disponibilidade = false
+
+let Livros = [livroUm, livroDois, livroTres]
